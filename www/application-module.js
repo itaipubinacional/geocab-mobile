38862f680c322520f16868dc55e3b0b2dfb455f7
Emanuel <emanuel.fonseca@eits.com.br>
2016-12-08 15:50:51 -0200

    Resolvendo erro de url invertida para xCode iOS, removidos comentários desnecessários do código

--- www/application-module.js
+++ www/application-module.js
@@ -8,15 +8,15 @@
    * Desenvolvimento
    */
     // module.constant('$API_ENDPOINT', 'http://192.168.20.108:8080/geocab');
-  module.constant('$API_ENDPOINT', 'http://geocab.sbox.me');
   /**
    * Homologação
    */
-  //module.constant('$API_ENDPOINT', 'http://chi554a:8080/geocab');
+    //module.constant('$API_ENDPOINT', 'http://chi554a:8080/geocab');
+    // module.constant('$API_ENDPOINT', 'http://geocab.sbox.me');
   /**
    * Produção
    */
-  //module.constant('$API_ENDPOINT', 'https://geocab.itaipu.gov.br');
+  module.constant('$API_ENDPOINT', 'https://geocab.itaipu.gov.br');
 
   /**
    *
