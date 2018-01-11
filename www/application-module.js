6beed48024e2ef3bea9c1e81cae6665d67c97ce8
Eduardo Niehues <eduardo@edanni.io>
2018-01-11 17:14:45 -0200

    Correção do endereço da aplicação

--- www/application-module.js
+++ www/application-module.js
@@ -11,13 +11,13 @@
   /**
    * Homologação
    */
-    module.constant('$API_ENDPOINT', 'http://chi554a:8080/geoitaipu');
+  //  module.constant('$API_ENDPOINT', 'http://chi554a:8080/geoitaipu');
     // module.constant('$API_ENDPOINT', 'http://geocab.sbox.me');
 
   /**
    * Produção
    */
-  //module.constant('$API_ENDPOINT', 'https://geocab.itaipu.gov.br');
+  module.constant('$API_ENDPOINT', 'https://geocab.itaipu.gov.br');
 
   /**
    *
